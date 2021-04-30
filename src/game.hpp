9d867bb36c8520c2f4b21f22c212908b8f887ac7
Huggets <48413879+Huggets@users.noreply.github.com>
2021-04-30 17:33:08 +0200

    Moving all code of engine into game
    - engine.hpp and engine.cpp no longer exists. Their code have moved into
    game.hpp and game.cpp
    
    - adding some new things

--- src/game.hpp
+++ src/game.hpp
@@ -1,35 +1,85 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-#include "engine.hpp"
+#include <SFML/Graphics.hpp>
+#include "world.hpp"
+#include "character.hpp"
 
 /*
    Represent the whole game.
-
-   It contains the engine that handles almost everything (loading ressources,
-   drawing on the window, managing characters…)
    */
 class Game {
     public:
         Game();
-        ~Game();
 
         /*
-           Main function of the game that call each frame function of the engine
-           in an infinite loop.
+           Start the game.
            */
-        void run();
+        void run(bool profile, char *profileConfigName);
 
     protected:
         /*
-           The game engine that handles almost everything.
+           Method that will be executed each frame of the game.
            */
-        Engine _engine;
+        inline void _frame();
 
         /*
-           Indicate whether the game is running or not.
+           The main window that display the game.
            */
-        bool _stillRunning;
+        sf::RenderWindow _window;
+
+        /*
+           Store the texture of a person.
+           */
+        sf::Texture _personTexture;
+
+        /*
+           The font used to display text.
+           */
+        sf::Font _font;
+
+        /*
+           Store the coordinates of the character the player control.
+           */
+        sf::Text _playerCoordinatesText;
+
+        /*
+           Store the the start time of the current _frame function that is being
+           executed.
+           */
+        clock_t _startTime;
+
+        /*
+           Store the the start time of the last _frame function that was
+           executed.
+           */
+        clock_t _lastTime;
+
+        /*
+           These two variables are used to avoid the update of the fps counter
+           each frame.
+           Look at the cpp file for more information.
+           */
+        int _fpsTime;
+        int _fpsTimeTmp;
+
+        /*
+           Point to the character the user controls.
+           */
+        Character * _playerCharacter;
+
+        /*
+           Defining two person for now.
+
+           TODO Search for an optimized way to create and handle many characters.
+           */
+        Character _person1;
+        Character _person2;
+
+        /*
+           The world we play.
+           */
+        World _world;
 };
 
 #endif
