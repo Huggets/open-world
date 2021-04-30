d11cce0326f99df98b48954dfeeb504792f1af72
Huggets <48413879+Huggets@users.noreply.github.com>
2021-04-30 17:30:02 +0200

    Changing the class into a namespace

--- src/worldfileparser.cpp
+++ src/worldfileparser.cpp
@@ -1,14 +1,18 @@
 #include <fstream>
+#include <iostream>
+#include <queue>
 #include "worldfileparser.hpp"
 
-void WorldFileParser::parse(std::string fileName) {
+void worldFileParser::parse(std::string fileName) {
     std::fstream worldFile;
     char c;
+    std::string word;
 
     worldFile.open(fileName, std::fstream::in);
     c = worldFile.get();
-    
     while (c != EOF) {
+        word += c;
+//        if (c == ' ' or 
         c = worldFile.get();
     }
 }
