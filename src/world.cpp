8581b20fcab993717b32802e7af8cf6543d23149
Huggets <48413879+Huggets@users.noreply.github.com>
2021-05-02 18:45:37 +0200

    Filling class with new methods and new attributes

--- src/world.cpp
+++ src/world.cpp
@@ -1,4 +1,37 @@
 #include "world.hpp"
+#include <iostream>
 
-World::World() {
+World::World(
+        int width,
+        int height,
+        int tileArray[],
+        std::unordered_map<int, std::string>* tileConverter
+        ) :
+    _width(width),
+    _height(height),
+    _tileArray(tileArray),
+    _tileConverter(tileConverter)
+{
+}
+
+int World::getWidth() const {
+    return _width;
+}
+
+int World::getHeight() const {
+    return _height;
+}
+
+int World::getTile(int x, int y) const {
+    if (x >= _width or y >= _height) {
+        return 0;
+    }
+    return _tileArray[x*_width+y];
+}
+
+std::string World::getTileRessource(int key) const {
+    if (_tileConverter->count(key) == 0) {
+        return std::string();
+    }
+    return _tileConverter->at(key);
 }
