fc1a831780af3871f87db2f0f7a2d2215011bfed
Huggets <huggets@protonmail.com>
2021-05-02 18:51:13 +0200

    The parse function now returns a World object.
    - The parse function also take a const char[] instead of a std::string.
    
    - Adding documentation to the parse function.

--- src/worldfileparser.hpp
+++ src/worldfileparser.hpp
@@ -2,9 +2,15 @@
 #define DEF_WORLDFILEPARSER
 
 #include <string>
+#include "world.hpp"
 
-namespace  worldFileParser {
-    void parse(std::string fileName);
+namespace worldFileParser {
+    /* 
+       Parses a world file and converts it into a World object.
+
+       Return the converted World.
+       */
+    World parse(const char fileName[]);
 }
 
 #endif
