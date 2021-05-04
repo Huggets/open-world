e1e228a2a42086c0a63ac8557abe82a0b6818720
Huggets <huggets@protonmail.com>
2021-05-04 16:27:07 +0200

    Changing message type from char[] to std::string

--- src/console.hpp
+++ src/console.hpp
@@ -5,22 +5,22 @@
 
 /*
    Contains functions that print message on the console.
-   */
+ */
 namespace console {
     /*
        Prints an error message in the console.
-       */
-    void error(const char message[]);
+     */
+    void error(const std::string& message);
 
     /*
        Prints a warning message in the console.
-       */
-    void warning(const char message[]);
+     */
+    void warning(const std::string& message);
 
     /*
        Prints an info message in the console.
-       */
-    void info(const char message[]);
+     */
+    void info(const std::string& message);
 }
 
 #endif
