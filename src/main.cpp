203d73aa71f1d4c4746ea1bd4afa87617eef631f
Huggets <huggets@protonmail.com>
2021-05-04 16:28:33 +0200

    Changing code style and removing PROFILE_FILE macro

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include "game.hpp"
 #include "console.hpp"
 
-#define PROFILE_FILE "data/profiles/profile.latest"
-
-int main(int argc, char ** argv) {
-    bool performanceProfile(false);
-    char profileConfigName[255] = PROFILE_FILE;
+int main(int argc, char** argv)
+{
+    bool savePerformance(false);
+    std::string performanceFile = "data/profiles/profile.latest";
 
     // TODO Finish the help menu
-    for (int i = 1 ; i < argc ; i++) {
+    for (int i = 1 ; i < argc ; i++)
+    {
         // If the argument is -h or --help we display the help menu
-        if (strcmp(argv[i], "--help") == 0 or strcmp(argv[i], "-h") == 0) {
+        if (strcmp(argv[i], "--help") == 0 or strcmp(argv[i], "-h") == 0)
+        {
             std::cout << "Display help" << std::endl;
             return 0;
-        
+        }
         // If the argument is -p or --profile we will store information about
         // performance in a file
-        } else if (
+        else if (
                 strcmp(argv[i], "--profile") == 0
                 or strcmp(argv[i], "-p") == 0
-                ) {
-            performanceProfile = true;
-            
+                )
+        {
+            savePerformance = true;
+
             // If there is an argument after -p or --profile we take it as the
             // profile file
-            if (i+1 < argc and strlen(argv[i+1]) < 256) {
-                strcpy(profileConfigName, argv[i+1]);
+            if (i < argc-1 and strlen(argv[i+1]) != 0)
+            {
+                performanceFile =  argv[i+1];
+                i++;
             }
         }
     }
 
     Game game;
-    game.run(performanceProfile, profileConfigName);
+    game.run(savePerformance, performanceFile);
 
     return 0;
 }
