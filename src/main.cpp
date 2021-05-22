dc43c4bf45ebd88e6e14fbc105b823c1e79236c9
Huggets <48413879+Huggets@users.noreply.github.com>
2021-05-22 16:45:12 +0200

    The command line can now takes a world file name
    - Adding the -w or --world argument to change the default world
      file name.

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,17 @@
 int main(int argc, char** argv)
 {
     bool savePerformance(false);
-    std::string performanceFile = "data/profiles/profile.latest";
+    std::string performanceFilename("data/profiles/profile.latest");
+    std::string worldFilename("data/worlds/world1.world");
 
     // TODO Finish the help menu
     for (int i = 1 ; i < argc ; i++)
     {
         // If the argument is -h or --help we display the help menu
-        if (strcmp(argv[i], "--help") == 0 or strcmp(argv[i], "-h") == 0)
+        if (
+                strcmp(argv[i], "--help") == 0 or
+                strcmp(argv[i], "-h") == 0
+           )
         {
             std::cout << "Display help" << std::endl;
             return 0;
@@ -31,14 +35,25 @@ int main(int argc, char** argv)
             // profile file
             if (i < argc-1 and strlen(argv[i+1]) != 0)
             {
-                performanceFile =  argv[i+1];
+                performanceFilename =  argv[i+1];
                 i++;
+            }
+        }
+        // The world to load
+        else if (
+                strcmp(argv[i], "--world") == 0
+                or strcmp(argv[i], "-w") == 0
+                )
+        {
+            if (i < argc-1 and strlen(argv[i+1]) != 0)
+            {
+                worldFilename = argv[i+1];
             }
         }
     }
 
-    Game game;
-    game.run(savePerformance, performanceFile);
+    Game game(worldFilename);
+    game.run(savePerformance, performanceFilename);
 
     return 0;
 }
