806e9988dbf88fbc43b3e41c25ced040b4668d97
Huggets <48413879+Huggets@users.noreply.github.com>
2021-05-29 23:04:38 +0200

    _tiles is now tiles and is in the public scope
    - isTileVoid and drawTile have been removed since its now possible
      to have the same behavior with a direct call with the tiles attributes.

--- src/worldpiece.cpp
+++ src/worldpiece.cpp
@@ -2,17 +2,6 @@
 
 WorldPiece::WorldPiece(
         std::unique_ptr<Tile[][WORLDPIECE_SIZE]>& tiles) :
-    _tiles(std::move(tiles))
+    tiles(std::move(tiles))
 {
-}
-
-bool WorldPiece::isTileVoid(unsigned int x, unsigned int y) const
-{
-    return _tiles[x][y].isVoid();
-}
-
-
-void WorldPiece::drawTile(sf::RenderWindow& window, unsigned int x, unsigned int y) const
-{
-    _tiles[x][y].draw(window);
 }
--- src/worldpiece.hpp
+++ src/worldpiece.hpp
@@ -12,20 +12,9 @@ class WorldPiece{
                 std::unique_ptr<Tile[][WORLDPIECE_SIZE]>& tiles);
 
         /*
-           Returns true if the tile at the corresponding coordinates
-           is a void tile.
-         */
-        bool isTileVoid(unsigned int x, unsigned int y) const;
-
-        /*
-           Draws the tile at the corresponding coordinates.
-         */
-        void drawTile(sf::RenderWindow& window, unsigned int x, unsigned int y) const;
-    protected:
-        /*
            Tiles contained in the WorldPiece
          */
-        std::unique_ptr<Tile[][WORLDPIECE_SIZE]> _tiles;
+        std::unique_ptr<Tile[][WORLDPIECE_SIZE]> tiles;
 };
 
 #endif
